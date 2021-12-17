ac764e7233f7e88135f14e532abfdc796244ab5f
jeannlucas <jeann.lucas@outlook.com>
2021-12-17 10:32:31 -0300

    Projeto final Clone Netflix

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,17 @@
-$('.owl-carousel').owlCarousel({
+/* Implementação Carousel Autoplay */ 
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
     loop:true,
     margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
 })
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
